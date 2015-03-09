efcc718323d14b422911ab68c8842f4dbf9de3ed
hengtianbx <bx@hengtiansoft.com>
2015-03-09 16:56:50 +0800

    Midified aspect to annotation

--- b2attr-service/src/main/java/com/ht/b2attr/b2attr_service/aspects/LogAspect.java
+++ b2attr-service/src/main/java/com/ht/b2attr/b2attr_service/aspects/LogAspect.java
@@ -1,30 +1,39 @@
 package com.ht.b2attr.b2attr_service.aspects;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
+@Aspect
 public class LogAspect {
+	@Pointcut("execution(* com.ht.b2attr.b2attr_service.SocketClient.receive(String)) && args(msg)")
+	public void receive(String msg) {
+	}
 
-	public void before()
-	{
-		System.out.println("before method.");		
+	@Before("receive(msg)")
+	public void before(String msg) {
+		System.out.println("before method." + msg);
 	}
-	
-	public void after()
-	{
-		System.out.println("after method.");		
+
+	@After("receive(msg)")
+	public void after(String msg) {
+		System.out.println("after method.");
 	}
-	
-	public void monitorReceive(ProceedingJoinPoint joinpoint)
-	{
-		System.out.println("monitor with aop around start..." );
-		System.out.println("parameters:" + joinpoint.getArgs()[0].toString() );
+
+	@Around("receive(msg)")
+	public void monitorReceive(ProceedingJoinPoint joinpoint,String msg) {
+		System.out.println("monitor with aop around start...");
+		System.out.println("parameters:" + joinpoint.getArgs()[0].toString());
 		try {
 			joinpoint.proceed();
 		} catch (Throwable e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		System.out.println("monitor with aop around start...");
 	}
 }
